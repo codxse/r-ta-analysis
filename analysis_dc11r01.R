c95ab980d07972e6622599f533059409011ed243
Nadiar AS <codxse@gmail.com>
2016-02-19 15:57:18 +0700

    moving to dplyr

--- analysis_dc11r01.R
+++ analysis_dc11r01.R
@@ -6,8 +6,12 @@
 #   tahun, pk_belum_ditempatkan_awal_tahun, pk_terdaftar, pk_ditempatkan, pk_dihapus,
 #   lk_belum_dipenuhi, lk_terdaftar, lk_dipenuhi, lk_dihapus, lk_ada_untuk_i_ii
 
+getwd()
+setwd('~/Workspaces/r-ta-analysys')
+library('dplyr')
+
 # buka csv
-fpath = file.path('~/Workspaces/r-ta-analysys/rawdata/dc11_data_ketenaga_kerjaan/R01_PEREKONOMIAN_ikhtisar_statistik_antar_kerja_2010_2012.csv')
+fpath = file.path('rawdata/dc11_data_ketenaga_kerjaan/R01_PEREKONOMIAN_ikhtisar_statistik_antar_kerja_2010_2012.csv')
 df <- read.csv(fpath,stringsAsFactors = FALSE)
 df$jumlah <- replace(df$jumlah, c(31,35), 0)
 df$tahun <- as.character(df$tahun)
@@ -15,46 +19,52 @@ df$jumlah <- as.numeric(df$jumlah)
 
 ## subset row yang sama
 # Pencari Kerja yang belum ditempatkan Awal Tahun
-pk1 <- subset(df,df$rincian_indikator == 'Pencari Kerja yang belum ditempatkan Awal Tahun')
-pk1 <- subset(pk1,select = -c(2,3))
+pk1 <- filter(df,rincian_indikator == 'Pencari Kerja yang belum ditempatkan Awal Tahun') %>%
+       select(-c(2,3))
 
 # Pencari Kerja yang Terdaftar
-pk2 <- subset(df,df$rincian_indikator == 'Pencari Kerja yang Terdaftar')
-pk2 <- subset(pk2,select = -c(2,3))
+pk2 <- filter(df,rincian_indikator == 'Pencari Kerja yang Terdaftar') %>%
+       select(-c(2,3))
 
 # Pencari Kerja yang ditempatkan
-pk3 <- subset(df,df$rincian_indikator == 'Pencari Kerja yang ditempatkan')
-pk3 <- subset(pk3,select = -c(2,3))
+pk3 <- filter(df,rincian_indikator == 'Pencari Kerja yang ditempatkan') %>%
+       select(-c(2,3))
 
 # Pencari Kerja yang dihapus
-pk4 <- subset(df,df$rincian_indikator == 'Pencari Kerja yang dihapus')
-pk4 <- subset(pk4,select = -c(2,3))
+pk4 <- filter(df,rincian_indikator == 'Pencari Kerja yang dihapus') %>%
+       select(-c(2,3))
 
 # Lowongan yang belum dipenuhi
-lk1 <- subset(df,df$rincian_indikator == 'Lowongan yang belum dipenuhi')
-lk1 <- subset(lk1,select = -c(2,3))
+lk1 <- filter(df,rincian_indikator == 'Lowongan yang belum dipenuhi') %>%
+       select(-c(2,3))
 
 # Lowongan yang terdaftar
-lk2 <- subset(df,df$rincian_indikator == 'Lowongan yang terdaftar')
-lk2 <- subset(lk2,select = -c(2,3))
+lk2 <- filter(df,rincian_indikator == 'Lowongan yang terdaftar') %>% 
+       select(-c(2,3))
 
 # Lowongan yang dipenuhi
-lk3 <- subset(df,df$indikator == 'Lowongan yang dipenuhi')
-lk3 <- subset(lk3,select = -c(2,3))
+lk3 <- filter(df,indikator == 'Lowongan yang dipenuhi') %>%
+       select(-c(2,3))
 
 # Lowongan Yang dihapus
-lk4 <- subset(df,df$indikator == 'Lowongan Yang dihapus')
-lk4 <- subset(lk4,select = -c(2,3))
+lk4 <- filter(df,indikator == 'Lowongan Yang dihapus') %>%
+       select(-c(2,3))
 
 # Lowongan Yang Ada (I-II)
-lk5 <- subset(df,df$indikator == 'Lowongan Yang Ada (I-II)')
-lk5 <- subset(lk5,select = -c(2,3))
+lk5 <- filter(df,indikator == 'Lowongan Yang Ada (I-II)') %>%
+       select(-c(2,3))
+
+## Buat vector Date (%Y)
+df2_date <- c()
+index_year <- 1
+for (year in 2012:2009) {
+  df2_date[index_year] <- paste0(year,'/01/01')
+  index_year <- index_year + 1
+}
+df2_date <- as.Date(df2_date,format='%Y/%m/%d')
+rm(index_year,year)
 
 ## Buat data.frame baru, join semua lk dan pk
-df2_date <- c(as.Date(paste0(2012,'-01-01')),
-              as.Date(paste0(2011,'-01-01')),
-              as.Date(paste0(2010,'-01-01')),
-              as.Date(paste0(2009,'-01-01')))
 df2 <- data.frame(df2_date,pk1$jumlah,pk2$jumlah,pk3$jumlah,
                   pk4$jumlah,lk1$jumlah,lk2$jumlah,lk3$jumlah,lk4$jumlah,lk5$jumlah)
 df2_head <- c('tahun',
