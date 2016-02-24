a30ec3153f7819e531e03a207a546c5e8e87acab
Nadiar AS <codxse@gmail.com>
2016-02-24 14:20:06 +0700

    Add residual sum square and unit

--- analysis_dc11r02.R
+++ analysis_dc11r02.R
@@ -25,29 +25,68 @@ df_no_outlier <- df %>%
 ## Buat Model Linear ggplot2
 # UMP vs % Kenaikan tahun sebelumnya
 # lm(df_no_outlier$ump ~ df_no_outlier$kenaikan_ump)
+# summary(lm(df_no_outlier$ump ~ df_no_outlier$kenaikan_ump))
 # compute_model_prediction(df_no_outlier, ump ~ kenaikan_ump, model='lm', se=TRUE)
 # cor(df_no_outlier$ump,df_no_outlier$kenaikan_ump)
+lm1_res <- ggplot(df_no_outlier,aes(x=kenaikan_ump,y=ump)) +
+  geom_point() +
+  geom_smooth(method='lm',se=FALSE,aes(col='red')) +
+  geom_segment(aes(x=kenaikan_ump,
+                   y=ump,
+                   xend=kenaikan_ump,
+                   yend=fitted(lm(ump~kenaikan_ump,data=df_no_outlier)))) +
+  xlab('% Kenaikan UMP Tahun Sebelumnya') +
+  ylab('UMP (Rp)') +
+  guides(col=F)
+
+# Invoke
+lm1_res
+
+# ggplot(df_no_outlier,aes(x=kenaikan_ump,y=ump)) +
+#   geom_point() +
+#   xlab('% Kenaikan UMP Tahun Sebelumnya') +
+#   ylab('UMP (Rp)') 
+
 lm1 <- ggplot(df_no_outlier,aes(x=kenaikan_ump,y=ump)) +
   geom_point() +
-  geom_smooth(method='lm',se=TRUE,aes(col='blue')) +
+  geom_smooth(method='lm',se=F,aes(col='red')) +
   xlab('% Kenaikan UMP Tahun Sebelumnya') +
-  ylab('UMP') +
-  scale_color_manual('f(x)',values='blue',labels='y = 3504x + 909588')
+  ylab('UMP (Rp)')  +
+  scale_color_manual('f(x)',values='red',labels='y = 3504x + 909588')
 
-## plot interactive
 lm1
+## plot interactive
 # ggplotly(lm1)
 
 # UMP vs % inflasi tahun sebelumnya
 # lm(df_no_outlier$ump ~ df_no_outlier$inflasi)
 # compute_model_prediction(df_no_outlier, ump ~ inflasi, model='lm', se=TRUE)
 # cor(df_no_outlier$ump,df_no_outlier$inflasi)
+lm2_res <- ggplot(df_no_outlier,aes(x=inflasi,y=ump)) +
+  geom_point() +
+  geom_smooth(method='lm',se=FALSE,aes(col='blue')) +
+  geom_segment(aes(x=inflasi,
+                   y=ump,
+                   xend=inflasi,
+                   yend=fitted(lm(ump~inflasi,data=df_no_outlier)))) +
+  xlab('% Inflasi') +
+  ylab('UMP (Rp)') +
+  guides(col=F)
+
+# Invoke
+lm2_res
+
+# ggplot(df_no_outlier,aes(x=inflasi,y=ump)) +
+#   geom_point() +
+#   xlab('% Inflasi') +
+#   ylab('UMP (Rp)') 
+
 lm2 <- ggplot(df_no_outlier,aes(x=inflasi,y=ump)) +
   geom_point() +
-  geom_smooth(method='lm',se=TRUE, aes(col='blue'))  +
-  xlab('% Inflasi Tahun Sebelumnya') +
-  ylab('UMP') +
-  scale_color_manual('f(x)',values='blue',labels='y = -50134x + 1331398')
+  geom_smooth(method='lm',se=FALSE, aes(col='red'))  +
+  xlab('% Inflasi') +
+  ylab('UMP (Rp)') +
+  scale_color_manual('f(x)',values='red',labels='y = -50134x + 1331398')
 
 ## plot interactive
 lm2
@@ -62,10 +101,10 @@ ggplot(df, aes(x=tahun,y=ump/10000,col='red',alpha=0.5)) +
   geom_point(aes(x=tahun,y=inflasi,col='blue',alpha=0.5)) + 
   geom_path(aes(x=tahun,y=inflasi,col='blue')) +
   xlab('Tahun') +
-  ylab('UMP / 10.000') +
+  ylab('UMP (Rp) / 10.000') +
   scale_colour_manual('Keterangan',
                       values=c('red','darkgreen','blue'),
-                      labels=c('UMP','% Naik UMP Sebelumnya','% Infasi Sebelumnya')) +
+                      labels=c('UMP (Rp)','% Naik UMP Sebelumnya','% Infasi Sebelumnya')) +
   guides(size=FALSE,alpha=FALSE)
 
 ## Using tidyr
