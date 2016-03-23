25d246521c0031eda48ead79dcacb9e19c1d49e3
Nadiar AS <codxse@gmail.com>
2016-03-23 22:17:23 +0700

    data viz dc12r06 pie chart

--- analysis_dc12r06.R
+++ analysis_dc12r06.R
@@ -17,7 +17,7 @@ df$bulan <- as.Date(paste0('2012/',df$bulan,'/01'))
 # Line chart as continue
 names(df) <- c('Komponen','Tanggal','Inflasi')
 
-ggplot(df, aes(x=Tanggal,y=Inflasi)) +
+line_ <- ggplot(df, aes(x=Tanggal,y=Inflasi)) +
   geom_line(aes(color=Komponen),size=1) +
   geom_point(aes(color=Komponen),
              size=4,
@@ -28,4 +28,37 @@ ggplot(df, aes(x=Tanggal,y=Inflasi)) +
   ylab('Inflasi (%)') +
   xlab('Bulan')
 
-# Pie chart as categorical
+line_
+
+# Pie Chart As Chategorical
+df.tab <- data.frame('Komponen'=c('Umum',
+                                  'Bukan Makanan',
+                                  'Makanan jadi, Minuman, Rokok dan tembakau',
+                                  'Perumahan, Air, Listrik, Gas, dan Bahan bakar',
+                                  'Sandang',
+                                  'Kesehatan',
+                                  'Pendidikan, Rekreasi dan Olah Raga',
+                                  'Transportasi, Komunikasi dan Jasa Keuangan'),
+                     'Inflasi'=c(sum(subset(df, df$Komponen == 'Umum')$Inflasi),
+                                 sum(subset(df, df$Komponen == 'Bukan Makanan')$Inflasi),
+                                 sum(subset(df, df$Komponen == 'Makanan jadi, Minuman, Rokok dan tembakau')$Inflasi),
+                                 sum(subset(df, df$Komponen == 'Perumahan, Air, Listrik, Gas, dan Bahan bakar')$Inflasi),
+                                 sum(subset(df, df$Komponen == 'Sandang')$Inflasi),
+                                 sum(subset(df, df$Komponen == 'Kesehatan')$Inflasi),
+                                 sum(subset(df, df$Komponen == 'Pendidikan, Rekreasi dan Olah Raga')$Inflasi),
+                                 sum(subset(df, df$Komponen == 'Transportasi, Komunikasi dan Jasa Keuangan')$Inflasi)))
+
+df.tab$Inflasi <- df.tab$Inflasi * 100
+df.tab$Persen <- df.tab$Inflasi/(sum(df.tab$Inflasi)/100)
+df.tab$Komponen <- paste0(df.tab$Komponen," (",
+                          format(round(df.tab$Persen, 2), nsmall=2)," %)")
+  
+pie_ <- ggplot(df.tab, aes(x='',y=Persen,fill=Komponen)) +
+  geom_bar(width=10,stat='identity') +
+  coord_polar('y', start=0) +
+  ggtitle("Persentasi Komponen Inflasi DKI Jakarta\nBulan Januari - Juni 2012") +
+  theme(plot.title=element_text(face='bold',size=15)) +
+  scale_x_discrete('') +
+  xlab('Komponen (%)')
+
+pie_
