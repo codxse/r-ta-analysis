586889476dd830f4a359f7dc855993b872b13f77
Nadiar AS <codxse@gmail.com>
2016-03-24 21:41:16 +0700

    data viz dc12r08 done

---
+++ analysis_dc12r08.R
@@ -0,0 +1,40 @@
+## dc12_data_ekonomi
+## R08_PEREKONOMIAN_struktur_ekonomi_jakarta_berdasarkan_sektor_2006_2012.csv
+
+getwd()
+setwd('~/Workspaces/r-ta-analysys')
+rm(list=ls())
+library(tidyr)
+library(ggplot2)
+library(dplyr)
+
+# buka csv
+fpath = file.path('rawdata/dc12_data_ekonomi/R08_PEREKONOMIAN_struktur_ekonomi_jakarta_berdasarkan_sektor_2006_2012.csv')
+df <- read.csv(fpath,stringsAsFactors = FALSE)
+df$tahun <- as.Date(paste0(df$tahun,'/01/01'))
+df$jenis_sektor <- factor(df$jenis_sektor,
+                          order=TRUE,
+                          levels=c('Primer (Pertanian/Pertambangan)',
+                                     'Sekunder (Industri/ Listrik/ Air/ Bangunan)',
+                                     'Tersier (Perdagangan/Angkutan/Keuangan/Jasa)'))
+df$persen_kontribusi <- as.numeric(df$persen_kontribusi)
+
+df.viz <- df
+names(df.viz) <- c('Tahun','Sektor','Kontribusi')
+
+# Bar Percentage graph
+
+df.viz <- df.viz %>% arrange(Sektor)
+
+bar_ <- ggplot(df.viz, aes(x=Tahun)) +
+  geom_bar(position='fill',
+           stat='identity',
+           aes(fill=Sektor,
+               y=Kontribusi)) +
+  scale_y_continuous(labels=scales::percent) +
+  geom_text(aes(y=Kontribusi/100,
+                label=paste(Kontribusi,'%'))) +
+  ggtitle("Struktur Ekonomi Jakarta Berdasarkan Sektor") +
+  theme(plot.title=element_text(face='bold',size=15))
+
+bar_
