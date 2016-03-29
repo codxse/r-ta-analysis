0c3808fc7f7156e2c2e3ecc1aae09cb527c8e138
Nadiar AS <codxse@gmail.com>
2016-03-29 10:30:18 +0700

    remove unnecessary library and plot dc12r03

--- analysis_dc12r03.R
+++ analysis_dc12r03.R
@@ -10,8 +10,10 @@ setwd('~/Workspaces/r-ta-analysys')
 rm(list=ls())
 library(dplyr)
 library(ggplot2)
-library(plotly)
 library(tidyr)
+
+# remove scientific numeric
+options(scipen=999)
 
 # buka csv
 fpath = file.path('rawdata/dc12_data_ekonomi/R03_PEREKONOMIAN_laju_pertumbuhan_ekonomi_jakarta_nasional_2006_2012.csv')
@@ -21,7 +23,7 @@ df <- read.csv(fpath,stringsAsFactors = FALSE) %>%
 title <- c('Laju Pertumbuhan Ekonomi Jakarta vs. Nasional')
 x <- c('Pertumbuhan Ekonomi Jakarta (%)','Tahun')
 y <- c('Pertumbuhan Ekonomi Nasional (%)')
-model_pred <- 'y = 1.1119x - 0.9794'
+model_pred <- 'f(x) = 1.1119x - 0.9794'
 
 # Hapus Outlier
 # No Outlier
@@ -29,12 +31,16 @@ model_pred <- 'y = 1.1119x - 0.9794'
 ## Using tidyr
 df.tidy <- gather(df, variable, persen, -tahun)
 
-line1 <- ggplot(df.tidy, aes(x=tahun, y=persen,col=variable)) +
+line1_ <- ggplot(df.tidy, aes(x=tahun, y=persen,col=variable)) +
   geom_line() +
   scale_color_manual('Keterangan',
                      values=c('red','blue'),
                      labels=c(x[1],y[1])) +
-  ggtitle(title[1])
+  labs(x='Tahun',
+       y='Persen (%)') +
+  ggtitle('Laju Pertumbuhan Ekonomi Jakarta vs. Nasional') +
+  theme(plot.title=element_text(face="bold", size=15))
+line1_
 
 ## Buat Model Linear ggplot2
 # Pertumbuhan Ekonomi Jakarta vs. Nasional
@@ -45,15 +51,17 @@ line1 <- ggplot(df.tidy, aes(x=tahun, y=persen,col=variable)) +
 # cor(df$persen_tumbuh_nasional,df$persen_tumbuh_jakarta)
 
 ## Scatter Plot
-plot1 <- ggplot(df,aes(x=persen_tumbuh_jakarta,
+plot1_ <- ggplot(df,aes(x=persen_tumbuh_jakarta,
                         y=persen_tumbuh_nasional)) +
   geom_point() + 
-  xlab(x[1]) +
-  ylab(y[1]) +
-  ggtitle(title[1])
+  labs(x=x[1],
+       y=y[1]) +
+  ggtitle('Plot Laju Pertumbuhan Ekonomi Jakarta vs. Nasional') +
+  theme(plot.title=element_text(face="bold", size=15))
+plot1_
 
 ## Resedual y_pred - kenaikan_ump
-ggplot(df,aes(x=persen_tumbuh_jakarta,
+plotRes_ <- ggplot(df,aes(x=persen_tumbuh_jakarta,
               y=persen_tumbuh_nasional)) +
   geom_point() +
   geom_smooth(method='lm',se=FALSE,aes(col='red')) +
@@ -62,34 +70,39 @@ ggplot(df,aes(x=persen_tumbuh_jakarta,
                    xend=persen_tumbuh_jakarta,
                    yend=fitted(lm(persen_tumbuh_nasional~persen_tumbuh_jakarta,
                                   data=df)))) +
-  xlab(x[1]) +
-  ylab(y[1]) +
-  ggtitle(title[1]) +
+  labs(x=x[1],
+       y=y[1]) +
+  ggtitle('Plot Laju Pertumbuhan Ekonomi Jakarta vs. Nasional') +
+  theme(plot.title=element_text(face="bold", size=15)) +
   guides(col=F)
+plotRes_
 
 ## Linear Model without SE
 # fit <- lm(df$persen_tumbuh_nasional ~ df$persen_tumbuh_jakarta)
 # library(ggvis)
 # compute_model_prediction(df,persen_tumbuh_nasional~persen_tumbuh_jakarta,model='lm', se=TRUE)
-ggplot(df,aes(x=persen_tumbuh_jakarta,
+ModelNoSe_ <- ggplot(df,aes(x=persen_tumbuh_jakarta,
               y=persen_tumbuh_nasional)) +
   geom_point() +
   geom_smooth(method='lm',se=FALSE,aes(col='red')) +
-  xlab(x[1]) +
-  ylab(y[1]) +
+  labs(x=x[1],
+       y=y[1]) +
   ggtitle(title[1]) +
-  scale_color_manual('f(x)',values='red',labels=model_pred)
+  theme(plot.title=element_text(face="bold", size=15)) +
+  scale_color_manual('Model',values='red',labels=model_pred)
+ModelNoSe_
 
 ## Linear Model with SE
-ggplot(df,aes(x=persen_tumbuh_jakarta,
+ModelSe_ <- ggplot(df,aes(x=persen_tumbuh_jakarta,
               y=persen_tumbuh_nasional)) +
   geom_point() +
   geom_smooth(method='lm',se=TRUE,aes(col='red')) +
-  xlab(x[1]) +
-  ylab(y[1]) +
+  labs(x=x[1],
+       y=y[1]) +
   ggtitle(title[1]) +
-  scale_color_manual('f(x)',values='red',labels=model_pred) +
-  guides(size=F)
+  theme(plot.title=element_text(face='bold',size=15)) +
+  scale_color_manual('Model',values='red',labels=model_pred)
+ModelSe_
 
 ## Confident Interval inflasi 7% (x=7)
 predict(lm(persen_tumbuh_nasional~persen_tumbuh_jakarta,
@@ -97,52 +110,37 @@ predict(lm(persen_tumbuh_nasional~persen_tumbuh_jakarta,
         data.frame(persen_tumbuh_jakarta=7),
         interval='confidence')
 
-## Interactive Plot
-model_1 <- ggplot(df,aes(x=persen_tumbuh_jakarta,
-                         y=persen_tumbuh_nasional),
-                  size=.1) +
-  geom_point() +
-  geom_smooth(method='lm',se=TRUE,aes(col='red')) +
-  xlab(x[1]) +
-  ylab(y[1]) +
-  ggtitle(title[1]) +
-  scale_color_manual('f(x)',values='red',labels=model_pred) +
-  guides(size=F)
-
-## Invoke
-# ggplotly(model_1)
-
 ## Data Visualization
 df$tahun <- as.Date(paste0(df$tahun,'/01/01'))
 
 # Model Prediction
-ggplot(df,aes(x=persen_tumbuh_jakarta,
+modelPred_ <- ggplot(df,aes(x=persen_tumbuh_jakarta,
               y=persen_tumbuh_nasional)) +
   geom_point() +
   geom_smooth(method='lm',se=TRUE,aes(col='red')) +
   xlab(x[1]) +
   ylab(y[1]) +
-  ggtitle('Model Prediction Pertumbuhan Ekonomi Jakarta vs. Nasional') +
+  ggtitle('Model Prediction\nPertumbuhan Ekonomi Jakarta vs. Nasional') +
   theme(plot.title=element_text(face="bold", size=15)) +
   scale_color_manual('Linear Model',values='red',labels=model_pred) +
   guides(size=F)
+modelPred_
 
 df.viz <- df
 names(df.viz) <- c('Tahun','Pertumbuhan Jakarta','Pertumbuhan Nasional')
 df.viz <-  gather(df.viz, variable, persen, -Tahun)
 df.viz$variable <- as.factor(df.viz$variable)
 
-ggplot(df.viz, aes(x=Tahun, y=persen)) +
+line_ <- ggplot(df.viz, aes(x=Tahun, y=persen)) +
   geom_line(aes(color=variable),size=1) +
   geom_point(aes(color=variable),
-             size=4,
+             size=3,
              shape=21,
              fill='white') +
   scale_size(range=c(5,10)) +
-  labs(color='Keterangan') +
-  ggtitle('Pertumbuhan Ekonomi Jakarta vs. Nasional') +
-  theme(plot.title=element_text(face="bold", size=15)) +
-  ylab('Pertumbuhan (%)') +
-  xlab('Tahun')
-  
-
+  labs(color='Keterangan',
+       x='Tahun',
+       y='Pertumbuhan (%)') +
+  ggtitle('Pertumbuhan Ekonomi Jakarta vs. Nasional\nTahun 2006-2012') +
+  theme(plot.title=element_text(face="bold", size=15))
+line_
