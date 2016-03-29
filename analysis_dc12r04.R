4d42361157205d934d6b755f55f61216eac6d9c3
Nadiar AS <codxse@gmail.com>
2016-03-29 11:00:10 +0700

    remove unnecessary library and graph

--- analysis_dc12r04.R
+++ analysis_dc12r04.R
@@ -10,8 +10,10 @@ setwd('~/Workspaces/r-ta-analysys')
 rm(list=ls())
 library(dplyr)
 library(ggplot2)
-library(plotly)
 library(tidyr)
+
+# remove scientific numeric
+options(scipen=999)
 
 # buka csv
 fpath = file.path('rawdata/dc12_data_ekonomi/R04_PEREKONOMIAN_inflasi_jakarta_nasional_2006_2012.csv')
@@ -21,7 +23,7 @@ df <- read.csv(fpath,stringsAsFactors = FALSE) %>%
 title <- c('Tingkat Inflasi Jakarta dan Nasional')
 x <- c('Inflasi Jakarta (%)','Tahun')
 y <- c('Inflasi Nasional (%)')
-model_pred <- 'y = 0.9889x - 0.3296'
+model_pred <- 'f(x) = 0.9889x - 0.3296'
 
 # Hapus Outlier
 # No Outlier
@@ -29,12 +31,16 @@ model_pred <- 'y = 0.9889x - 0.3296'
 ## Using tidyr
 df.tidy <- gather(df, variable, persen, -tahun)
 
-line1 <- ggplot(df.tidy, aes(x=tahun, y=persen,col=variable)) +
-  geom_line() +
+line1_ <- ggplot(df.tidy, aes(x=tahun, y=persen,col=variable)) +
+  geom_line(size=1) +
   scale_color_manual('Keterangan',
                      values=c('red','blue'),
                      labels=c(x[1],y[1])) +
-  ggtitle(title[1])
+  labs(x='Tahun',
+       y='Inflasi (%)') +
+  ggtitle(title[1]) +
+  theme(plot.title=element_text(face="bold", size=15))
+line1_
 
 ## Buat Model Linear ggplot2
 # % Nasional vs % Jakarta
@@ -45,15 +51,17 @@ line1 <- ggplot(df.tidy, aes(x=tahun, y=persen,col=variable)) +
 # cor(df$inflasi_nasional, df$inflasi_jakarta)
 
 ## Scatter Plot
-plot1 <- ggplot(df,aes(x=inflasi_jakarta,
+plot1_ <- ggplot(df,aes(x=inflasi_jakarta,
                        y=inflasi_nasional)) +
   geom_point() + 
-  xlab(x[1]) +
-  ylab(y[1]) +
-  ggtitle(title[1])
+  labs(x=x[1],
+       y=y[1]) +
+  ggtitle(title[1]) +
+  theme(plot.title=element_text(face="bold", size=15))
+plot1_
 
 ## Resedual y_pred - inflasi_nasional
-ggplot(df,aes(x=inflasi_jakarta,
+plotRes_ <- ggplot(df,aes(x=inflasi_jakarta,
               y=inflasi_nasional)) +
   geom_point() +
   geom_smooth(method='lm',se=FALSE,aes(col='red')) +
@@ -62,34 +70,40 @@ ggplot(df,aes(x=inflasi_jakarta,
                    xend=inflasi_jakarta,
                    yend=fitted(lm(inflasi_nasional~inflasi_jakarta,
                                   data=df)))) +
-  xlab(x[1]) +
-  ylab(y[1]) +
-  ggtitle(title[1]) +
+  labs(x=x[1],
+       y=y[1]) +
+  ggtitle('Inflasi DKI Jakarta vs. Nasional') +
+  theme(plot.title=element_text(face="bold", size=15)) +
   guides(col=F)
+plotRes_
 
 ## Linear Model without SE
 # fit <- lm(df$inflasi_nasional ~ df$inflasi_jakarta)
 # library(ggvis)
 # compute_model_prediction(df,inflasi_nasional ~ inflasi_jakarta, model='lm', se=TRUE)
-ggplot(df,aes(x=inflasi_jakarta,
+modelNoSe_ <- ggplot(df,aes(x=inflasi_jakarta,
               y=inflasi_nasional)) +
   geom_point() +
   geom_smooth(method='lm',se=FALSE,aes(col='red')) +
-  xlab(x[1]) +
-  ylab(y[1]) +
-  ggtitle(title[1]) +
-  scale_color_manual('f(x)',values='red',labels=model_pred)
+  labs(x=x[1],
+       y=y[1]) +
+  ggtitle('Inflasi DKI Jakarta vs. Nasional') +
+  theme(plot.title=element_text(face="bold", size=15)) +
+  scale_color_manual('Model',values='red',labels=model_pred)
+modelNoSe_
 
 ## Linear Model with SE
-ggplot(df,aes(x=inflasi_jakarta,
+modelSe_ <- ggplot(df,aes(x=inflasi_jakarta,
               y=inflasi_nasional)) +
   geom_point() +
   geom_smooth(method='lm',se=TRUE,aes(col='red')) +
-  xlab(x[1]) +
-  ylab(y[1]) +
+  labs(x=x[1],
+       y=y[1]) +
   ggtitle(title[1]) +
-  scale_color_manual('f(x)',values='red',labels=model_pred) +
+  theme(plot.title=element_text(face="bold", size=15)) +
+  scale_color_manual('Model',values='red',labels=model_pred) +
   guides(size=F)
+modelSe_
 
 ## Confident Interval inflasi 7% (x=7)
 predict(lm(inflasi_nasional~inflasi_jakarta,
@@ -97,48 +111,35 @@ predict(lm(inflasi_nasional~inflasi_jakarta,
         data.frame(inflasi_jakarta=7),
         interval='confidence')
 
-## Interactive Plot
-model_1 <- ggplot(df,aes(x=inflasi_jakarta,
-                         y=inflasi_nasional),
-                  size=.1) +
-  geom_point() +
-  geom_smooth(method='lm',se=TRUE,aes(col='red')) +
-  xlab(x[1]) +
-  ylab(y[1]) +
-  ggtitle(title[1]) +
-  scale_color_manual('f(x)',values='red',labels=model_pred) +
-  guides(size=F)
-
-## Invoke
-# ggplotly(model_1)
-
 ## Data Visualization
 df$tahun <- as.Date(paste0(df$tahun,'/01/01'))
 
 # Model Prediction
-ggplot(df,aes(x=inflasi_jakarta,
+modelPred_ <- ggplot(df,aes(x=inflasi_jakarta,
               y=inflasi_nasional)) +
   geom_point() +
   geom_smooth(method='lm',se=TRUE,aes(col='red')) +
-  xlab(x[1]) +
-  ylab(y[1]) +
-  ggtitle('Model Prediction Inflasi DKI Jakarta vs. Nasional') +
+  labs(x=x[1],
+       y=y[1]) +
+  ggtitle('Model Prediction\nInflasi DKI Jakarta vs. Nasional') +
   theme(plot.title=element_text(face="bold", size=15)) +
-  scale_color_manual('Linear Model',values='red',labels=model_pred) +
+  scale_color_manual('Model',values='red',labels=model_pred) +
   guides(size=F)
+modelPred_
 
 df.viz <- df
 names(df.viz) <- c('Tahun', 'Inflasi Jakarta', 'Inflasi Nasional')
 df.viz <- gather(df.viz, key, value, -Tahun)
 
-ggplot(df.viz, aes(x=Tahun, y=value)) +
+line_ <- ggplot(df.viz, aes(x=Tahun, y=value)) +
   geom_line(aes(color=key),size=1) +
   geom_point(aes(color=key),
-             size=4,
+             size=3,
              shape=21,
              fill='white') +
-  labs(color='Keterangan') +
-  ggtitle('Inflasi DKI Jakarta vs. Nasional') +
-  theme(plot.title=element_text(face="bold", size=15)) +
-  ylab('Inflasi (%)') +
-  xlab('Tahun')
+  labs(color='Keterangan',
+       x='Tahun',
+       y='Inflasi (%)') +
+  ggtitle('Laju Inflasi DKI Jakarta dan Nasional\nTahun 2006-2012') +
+  theme(plot.title=element_text(face="bold", size=15))
+line_
