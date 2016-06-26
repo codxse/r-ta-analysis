baa3a7cddb30249fcc1319978b82c8e8d3da0bbd
Nadiar AS <codxse@gmail.com>
2016-06-26 15:12:18 +0700

    update remover and a way to automatic update

--- remover.py
+++ remover.py
@@ -1,16 +1,16 @@
 import re
 
-file = open('data.json', 'r')
+file = open('data.js', 'r')
 lines = file.readlines()
 file.close()
 
-file = open('data.json', 'w')
+file = open('data.js', 'w')
 for line in lines:
     if line[0] != '>':
         file.write(line)
 file.close()
 
-with open('data.json', 'r') as fin:
+with open('data.js', 'r') as fin:
     data = fin.read().splitlines(True)
-with open('data.json', 'w') as fout:
+with open('data.js', 'w') as fout:
     fout.writelines(data[1:])
